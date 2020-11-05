8c82d5f0a0f434e6125174b6958d200ca41ad5f3
root <root@Staz.localdomain>
2020-11-05 17:13:54 -0400

    terminado

--- myFile.c
+++ myFile.c
@@ -6,12 +6,14 @@
 int ler_chunk(char *arquivo,struct gen_chunk* tmp){
     FILE* arq_in = fopen(arquivo, "rb");
     struct IHDR_chunk* aux;
+    int cont = 1;
     fseek(arq_in, 8,SEEK_CUR);
     while(!feof(arq_in)){
         tmp = malloc(sizeof(struct gen_chunk));
         fread(tmp,sizeof(struct gen_chunk),1,arq_in);
+        printf("Lendo Chunk: %d\n", cont);
         printf("--> Tamanho:\t%d\n",ntohl(tmp -> tamanho));
-        printf("--> Tipo:\t%0.4s\n",tmp -> tipo);
+        printf("--> Tipo:\t%.4s\n",tmp -> tipo);
         if(tmp -> tipo[0] == 'I' && tmp -> tipo[3] == 'R'){
             aux = malloc(sizeof(struct IHDR_chunk));
             fread(aux,sizeof(struct IHDR_chunk),1,arq_in);
@@ -20,15 +22,16 @@ int ler_chunk(char *arquivo,struct gen_chunk* tmp){
             fseek(arq_in, 4,SEEK_CUR);
         }
         else if(tmp -> tipo[0] == 'I' && tmp -> tipo[3] == 'D'){
-            fseek(arq_in,0,SEEK_END);
+            return 3;
         }
         else{
             fseek(arq_in,ntohl(tmp -> tamanho) + 4,SEEK_CUR);
         }
-        free(aux);
-        free(tmp);
+        cont ++;
 
     }
+    free(aux);
+    free(tmp);
     fclose(arq_in);
     
 }
